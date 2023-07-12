36c3c6b879a8539737229def9b255783e005a127
TheDoctor-JI <yifanchangle@163.com>
2023-07-12 19:00:51 +0800

    Updated logging

--- grace_instantaneous_state_monitor.py
+++ grace_instantaneous_state_monitor.py
@@ -39,30 +39,10 @@ import robot_behav_fsm
 import human_speaking_fsm
 import turn_owner_fsm
 
-
-
-
-#Create Logger
-def setupLogger(file_log_level, terminal_log_level, logger_name, log_file_name):
-    log_formatter = logging.Formatter('%(asctime)s %(msecs)03d %(name)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s', 
-                                  datefmt='%d/%m/%Y %H:%M:%S')
-
-    f = open(log_file_name, "a")
-    f.close()
-    file_handler = logging.FileHandler(log_file_name)
-    file_handler.setFormatter(log_formatter)
-    file_handler.setLevel(file_log_level)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(log_formatter)
-    stream_handler.setLevel(terminal_log_level)
-
-    logger = logging.getLogger(logger_name)
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-    logger.setLevel( min(file_log_level,terminal_log_level) )#set to lowest
-
-    return logger
+#Misc
+sys.path.append(os.path.join(file_path, '..'))
+from CommonConfigs.grace_cfg_loader import *
+from CommonConfigs.logging import setupLogger
 
 #Respond to exit signal
 def handle_sigint(signalnum, frame):
@@ -73,14 +53,19 @@ def handle_sigint(signalnum, frame):
     
 class InstantaneousStateMonitor:
 
-    def __init__(self, config_data, nh_in = None):
+    def __init__(self, config_data, logger = None, nh_in = None):
         #miscellaneous
         signal(SIGINT, handle_sigint)
-        self.__logger = setupLogger(
-                    logging.DEBUG, 
-                    logging.INFO, 
-                    self.__class__.__name__,
-                    "./logs/log_" + datetime.now().strftime("%a_%d_%b_%Y_%I_%M_%S_%p"))
+
+        #Monitor uses its own logger (output to its own dir) or the input logger
+        if(logger == None):
+            self.__logger = setupLogger(
+                        logging.DEBUG, 
+                        logging.INFO, 
+                        self.__class__.__name__,
+                        os.path.join(file_path,"./logs/log_") + datetime.now().strftime("%a_%d_%b_%Y_%I_%M_%S_%p"))
+        else:
+            self.__logger = logger.getChild(self.__class__.__name__)
 
         self.__config_data = config_data
 
@@ -183,11 +168,7 @@ class InstantaneousStateMonitor:
 
 if __name__ == '__main__':
 
-    file_path = os.path.dirname(os.path.realpath(getsourcefile(lambda:0)))
-    sys.path.append(os.path.join(file_path, '..'))
-    from CommonConfigs.grace_cfg_loader import *
     grace_config = loadGraceConfigs()
-
     inst_state_monitor = InstantaneousStateMonitor(grace_config)
     inst_state_monitor.mainLoop()
 
